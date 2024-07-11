0979bb092782e28da1e3d5ab15ec43bd9bf4a0c1
Architsoni09 <architsoni2908@gmail.com>
2024-07-12 00:41:54 +0530

    Busy Buy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
 import Navbar from "./Component/Navbar/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import Home from "./Pages/Home";
 import CustomAllProductsContext from "./Contexts/AllProductContext";
 import FirebaseAuth from "./Firebase/FirebaseAuth";
 import CustomUserDetailsContext from "./Contexts/UserDetailsContext";
-import {ToastContainer} from "react-toastify";
-import {Bounce} from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import { Bounce } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {fas} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import Cart from "./Pages/Cart";
 import Orders from "./Pages/Orders";
-
 
 function App() {
     library.add(fas);
@@ -53,7 +52,7 @@ function App() {
 
     return (
         <>
-            <RouterProvider router={routes} />
+            <RouterProvider router={routes} basename={process.env.PUBLIC_URL} />
         </>
     );
 }
